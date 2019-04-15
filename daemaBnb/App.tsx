d5828c02530ee761171790a003de95d44df7fee0
khg0712 <k7120792@gmail.com>
2019-04-15 23:23:54 +0900

    [ADD] RN-ts project initial setting

---
+++ daemaBnb/App.tsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+
+type Props = {};
+export default class App extends Component<Props> {
+  render() {
+    return (
+      <View>
+        <Text>Hello world!</Text>
+      </View>
+    );
+  }
+}
