1fdbc5b38f72be285ca12ce65a8d055d98921250
khg0712 <k7120792@gmail.com>
2019-05-08 09:33:43 +0900

    MODIFY date-presenter with formatDate

--- daemaBnb/src/components/CustomDatePresenter.tsx
+++ daemaBnb/src/components/CustomDatePresenter.tsx
@@ -1,22 +1,21 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import formatDate from '../utils/formatDate';
+
 interface Props {
   startDate: Date;
   endDate: Date;
-  color?: string;
 }
 
-const dateFormatter = (start: Date, end: Date): string => {
-  return `${start.getMonth() + 1}월 ${start.getDate()}일 ~ ${end.getMonth() + 1}월 ${end.getDate()}일`;
-};
-
-const CustomDatePresenter: React.FC<Props> = ({ startDate, endDate, color = 'black' }) => {
+const color = 'black';
+const CustomDatePresenter: React.FC<Props> = ({
+  startDate,
+  endDate,
+}) => {
   return (
     <View>
-      <Text style={{ color }}>
-        { dateFormatter(startDate, endDate) }
-      </Text>
+      <Text style={{ color }}>{formatDate(startDate, endDate)}</Text>
     </View>
   );
 };
