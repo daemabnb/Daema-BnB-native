f12e7d3e954a22820bfcf3503d82db51bb23744d
khg0712 <k7120792@gmail.com>
2019-05-08 13:25:48 +0900

    MODIFY variaable name

--- daemaBnb/src/components/Divider.tsx
+++ daemaBnb/src/components/Divider.tsx
@@ -1,8 +1,8 @@
 import styled from 'styled-components/native';
 
-const Divider = styled.View`
+const CustomDivider = styled.View`
   border-bottom-width: 1;
   border-bottom-color: #ebebeb;
 `;
 
-export default Divider;
+export default CustomDivider;
