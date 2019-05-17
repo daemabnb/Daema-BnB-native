5e19f55025ffa099483b6c0dc5fce294700dd17b
khg0712 <k7120792@gmail.com>
2019-05-17 11:24:25 +0900

    MODIFY formatDate usage

--- daemaBnb/src/utils/formatDate.ts
+++ daemaBnb/src/utils/formatDate.ts
@@ -1,5 +1,3 @@
-export const formatDate = (startDate: Date, endDate: Date) => {
-  return `${startDate.getMonth() +
-    1}월 ${startDate.getDate()}일 ~ ${endDate.getMonth() +
-    1}월 ${endDate.getDate()}일`;
+export const formatDate = (currentDate: Date) => {
+  return `${currentDate.getMonth() + 1}월 ${currentDate.getDate()}일`;
 };
